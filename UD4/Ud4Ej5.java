a896f874910a8442d2f73fb2f645cf2fa756b138
Jordii <Jordibaspas07@gmail.com>
2026-05-19 22:55:18 +0200

    Add files via upload

---
+++ UD4/Ud4Ej5.java
@@ -0,0 +1,14 @@
+public class Ud4Ej5 {
+    public static void main(String[] args) throws Exception {
+        //Entorno
+        int millas, yardas;
+        float km, m;
+        //Algoritmo
+        millas=26;
+        yardas=385;
+        km=(float) (millas*1.60);
+        m=(float) (yardas*0.91);
+        km=km+(m/1000);
+        System.out.println("La maraton en km son: "+km+"kilometros");
+    }
+}
