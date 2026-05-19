acd6371c0cf0fb8fc463cf2a519f06759cff756f
Jordii <Jordibaspas07@gmail.com>
2026-05-19 22:56:39 +0200

    Add files via upload

---
+++ UD4/AEFBoletinUd4/Ud4Ej1.java
@@ -0,0 +1,5 @@
+public class Ud4Ej1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ UD4/AEFBoletinUd4/Ud4Ej2.java
@@ -0,0 +1,10 @@
+public class Ud4Ej2 {
+    public static void main(String[] args) throws Exception {
+        //Entorno:
+        int n1, n2;
+        //Algoritmo:
+        n1=3;
+        n2=5;
+        System.out.println("La suma de "+n1+" y "+n2+" es: "+(n1+n2));
+    }
+}
