07d11973ea23e14babe9342258cb967c16a88c24
Jordii <Jordibaspas07@gmail.com>
2026-05-19 23:44:38 +0200

    Add files via upload

---
+++ PROG/UD5/AEFProgramasAlternativas/Ud5Ej2.java
@@ -0,0 +1,14 @@
+public class Ud5Ej2 {
+    public static void main(String[] args) throws Exception {
+        //Entorno:
+        int n;
+        //Algoritmo
+        System.out.println("Introduzca el numero: ");
+        n=Leer.datoInt();
+        if (n%2!=0){
+            n=n*n;
+            System.out.println("El cuadrado es: "+n);
+        }
+
+    }
+}
