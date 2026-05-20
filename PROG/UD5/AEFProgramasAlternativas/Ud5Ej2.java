67ade153a743d157851e64eae9d4c0b5f1ae8dd0
Jordii <Jordibaspas07@gmail.com>
2026-05-20 21:56:04 +0200

    Update Ud5Ej2.java

--- PROG/UD5/AEFProgramasAlternativas/Ud5Ej2.java
+++ PROG/UD5/AEFProgramasAlternativas/Ud5Ej2.java
@@ -1,5 +1,5 @@
 public class Ud5Ej2 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         //Entorno:
         int n;
         //Algoritmo
